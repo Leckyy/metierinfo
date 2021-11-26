2d792707b971f759c47bcdd3621520fb6772a4e7
Leckyy <90345150+Leckyy@users.noreply.github.com>
2021-11-26 18:49:29 +0100

    Add files via upload

---
+++ qcm.js
@@ -0,0 +1,142 @@
+var speechstr='"';
+var A1= new String
+var A2= new String
+var A3= new String
+var A4= new String
+var A5= new String
+var A6= new String
+var A7= new String
+var A8= new String
+var A9= new String
+var A10= new String
+function MarkAnswers(){
+	var i=0;
+	t=10;
+	Answers=window.open("","",",");
+if (A1.toLowerCase()=="3")
+{
+i=i+1;
+}
+if (A2.toLowerCase()=="2")
+{
+i=i+1;
+}
+if (A3.toLowerCase()=="1")
+{
+i=i+1;
+}
+if (A4.toLowerCase()=="2")
+{
+i=i+1;
+}
+if (A5.toLowerCase()=="2")
+{
+i=i+1;
+}
+if (A6.toLowerCase()=="3")
+{
+i=i+1;
+}
+if (A7.toLowerCase()=="1")
+{
+i=i+1;
+}
+if (A8.toLowerCase()=="2")
+{
+i=i+1;
+}
+if (A9.toLowerCase()=="3")
+{
+i=i+1;
+}
+if (A10.toLowerCase()=="2")
+{
+i=i+1;
+}
+
+Answers.document.write("<B><center>Vous avez obtenue " + i.toString() + " bonnes réponses sur " + t.toString()+"</center></B>");
+if (A1.toLowerCase()=="3")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 1</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 1</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A2.toLowerCase()=="2")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 2</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 2</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A3.toLowerCase()=="1")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 3</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 3</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A4.toLowerCase()=="2")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 4</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 4</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A5.toLowerCase()=="2")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 5</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 5</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A6.toLowerCase()=="3")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 6</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 6</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A7.toLowerCase()=="1")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 7</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 7</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A8.toLowerCase()=="2")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 8</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 8</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+if (A9.toLowerCase()=="3")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#1FFF00>Question 9</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#FF0000>Question 9</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"Verdana"+speechstr+" COLOR=#000000> Mauvaise réponse ");
+}
+if (A10.toLowerCase()=="2")
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"@import url('https://fonts.googleapis.com/css2? family=Cabine&display=swap');"+speechstr+" COLOR=#1FFF00>Question 10</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"@import url('https://fonts.googleapis.com/css2? family=Cabine&display=swap');"+speechstr+" COLOR=#000000> Bonne réponse");
+}
+else
+{
+Answers.document.write("<p align=center><B><FONT SIZE=2 FACE="+speechstr+"@import url('https://fonts.googleapis.com/css2? family=Cabine&display=swap');"+speechstr+" COLOR=#FF0000>Question 10</B></FONT></B><FONT SIZE=2 FACE="+speechstr+"@import url('https://fonts.googleapis.com/css2? family=Cabine&display=swap');"+speechstr+" COLOR=#000000> Mauvaise réponse");
+}
+
+}
+function close(){
+	window.close();
+}
